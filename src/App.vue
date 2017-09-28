6787d9906d7b5bbb78bdd5aacba7d74d2cec3185
DESKTOP-JK6QKC1\Daniel Perez <daniel2213perez@gmail.com>
2017-09-28 02:01:08 -0500

    fixed por la url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://danper2213.github.io/danpermusic/dist/logo.png')
     h1 DanperMusic
     select(v-model="selectedCountry") 
       option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
