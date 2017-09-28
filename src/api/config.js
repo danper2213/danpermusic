3f16a95eb750e170e27f9c369af26f8cb67d47cd
DESKTOP-JK6QKC1\Daniel Perez <daniel2213perez@gmail.com>
2017-09-28 01:19:53 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default{
+    appname: 'danpermusic',
+    apiKey: '6c33b88810b8013ac71ff5f9872d2533',
+    secret: 'c78193fe6087e661a1767cdd95186f01',
+    regristeredTo: 'danper2213'
+}
+
+
+/* Application name 	danpermusic
+API key 	6c33b88810b8013ac71ff5f9872d2533
+Shared secret 	c78193fe6087e661a1767cdd95186f01
+Registered to 	danper2213 */
